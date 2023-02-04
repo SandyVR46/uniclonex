123e91fa045ec84d05c3aca036227c678f24433c
Sandra S S <sandrasureshkumar123@gmail.com>
2023-02-04 15:38:11 +0530

    testing--smart--contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,11 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    hardhat: {
+      forking: {
+        url: "https://eth-mainnet.g.alchemy.com/v2/IAmRmpHdkfS07TF1PHTFdAQguR-hgCDO",
+      },
+    },
+  }
 };
